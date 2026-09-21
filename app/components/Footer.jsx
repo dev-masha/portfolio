18c5efd86c07625d09b8227ea8b38ed004583f47
dev-masha <mashadev.git@gmail.com>
2026-09-21 21:50:02 +0500

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
   <div className="footer">
    <h2>Masha<span className="dot">.</span></h2>
    <p><a href="mailto:mashadev.git@gmail.com" style={{color:"black", textDecoration:"none", fontWeight:"bold"}}>mashadev.git@gmail.com</a></p>
-   <p style={{marginTop:"10px", fontSize:"11px"}}>© 2025 Masha. All rights reserved.</p>
+   <p style={{marginTop:"10px", fontSize:"11px"}}>© 2026 Masha. All rights reserved.</p>
   </div>
  )
 }
